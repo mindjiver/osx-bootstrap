9feb8b7df4fd711b0459b0d231ff0d2e87d9ba21
Olle Lundberg <geek@nerd.sh>
2014-10-15 01:21:11 +0200

    Homebrew install script has moved!

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,7 +15,7 @@ fi
 
 have_brew=$(which brew > /dev/null)
 if [ $? -gt 0 ]; then
-    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
     echo "Brew already installed, updating it"
     brew update
